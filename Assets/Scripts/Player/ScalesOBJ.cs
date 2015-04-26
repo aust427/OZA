cda1370e9055c3cfde2de024f4ca16a545a2ed34
aust427 <austriel27@gmail.com>
2015-04-26 15:35:02 -0500

    Added Scales + Ode to Joy
    -Ode to Joy is now a thing. Yay another song we can play!!
    -Added hella scales. Gonna add the other hella amount soon.
    -Apparently background sound does change with the key. Oh my. C#
    Major/Minor sounds beautiful.

--- Assets/Scripts/Player/ScalesOBJ.cs
+++ Assets/Scripts/Player/ScalesOBJ.cs
@@ -44,11 +44,21 @@ public class ScalesOBJ : MonoBehaviour
 
 
 	////// COMBO DEFINITIONS //////
+	public static Scale aMajor = new Scale ("A Major", 5, true, "A Major."); 
+	public static Scale aMinor = new Scale ("A Minor", 5, false, "A minor. We really need to think of puns like soon."); 
+	public static Scale bbMajor = new Scale ("A#/Bb Major", 6, true, "A# Major. Also known as Bb Major."); 
+	public static Scale bbMinor = new Scale ("A#/Bb Minor", 6, false, "A# minor. We really need to think of puns like soon."); 
+	public static Scale bMajor = new Scale ("B Major", 7, true, "B Major."); 
+	public static Scale bMinor = new Scale ("B Minor", 7, false, "B minor. We really need to think of puns like soon."); 
 	public static Scale cMajor = new Scale ("C Major", 8, true, "This is a C Major scale. Puns go here.");
 	public static Scale cMinor = new Scale ("C Minor", 8, false, "C minor needs a description, too.");
+	public static Scale dbMajor = new Scale ("C#/Db Major", 9, true, "C# Major. Also known as Db Major.");
+	public static Scale dbMinor = new Scale ("C#/Db Minor", 9, false, "C# minor. Also known as Db minor."); 
+	public static Scale dMajor = new Scale ("D Major", 10, true, "This is a D Major scale. Puns go here.");
+	public static Scale dMinor = new Scale ("D Minor", 10, false, "D minor needs a description, too.");
+	public static Scale ebMajor = new Scale ("D#/Eb Major", 11, true, "Eb Major. Also known as D# Major."); 
+	public static Scale ebMinor = new Scale ("D#/Eb Minor", 11, false, "Eb minor. Also known as D# minor."); 
 	
-
-
 	// The change key function
 	public void ChangeKey(Scale scale)
 	{
