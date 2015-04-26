9d8f0e5613de63094fcb5020a0cd81adb16d4949
Nick <nickolenz@comcast.net>
2015-04-26 14:32:56 -0500

    Customizable hit sounds added.

---
+++ Assets/Scripts/Enemy/CustomHitSound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomHitSound : MonoBehaviour {
+	public string customHitSoundName = "Generic/HitSound";
+	public AudioClip sound;
+	
+	void Awake ()
+	{
+		sound = Resources.Load ("Sounds/" + customHitSoundName) as AudioClip;
+	}
+}
--- Assets/Scripts/Projectiles/GenericNoteAttack.cs
+++ Assets/Scripts/Projectiles/GenericNoteAttack.cs
@@ -5,9 +5,28 @@ public class GenericNoteAttack : CustomProjectile {
 	/**
 	 * Damages the enemy on hit
 	 **/
+	
+	AudioClip hitSound;
+	
+	void Awake ()
+	{
+		hitSound = Resources.Load("Sounds/Generic/HitSound") as AudioClip;
+	}
+	
+	
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Enemy") {
 			DamageableObject enemy = other.gameObject.GetComponent<DamageableObject> ();
+			CustomHitSound customHitSound = other.gameObject.GetComponent<CustomHitSound>();
+			if (customHitSound == null)
+			{
+				AudioSource.PlayClipAtPoint(hitSound, other.transform.position);	// Default
+			}
+			else
+			{
+				hitSound = customHitSound.sound;
+				AudioSource.PlayClipAtPoint(hitSound, other.transform.position);	// Use custom
+			}
 			
 			int directionKnockback;
 			if (transform.position.x - enemy.transform.position.x > 0)
